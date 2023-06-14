98d94b68aa9aa4c1e20450a67de89996e0dfb95f
LEOWJSheng <118702346+LEOWJSheng@users.noreply.github.com>
2023-06-14 15:47:12 +0800

    Update viewMenu.java

--- Latest Version For All/viewMenu.java
+++ Latest Version For All/viewMenu.java
@@ -3,126 +3,118 @@ package JOJOLands.JOJO;
 import java.util.*;
 
 public class viewMenu extends Menu {
-    private String restaurant;
-    private ArrayList<Food> foodList;
     private String currentLocation;
+    private String foodName;
+    private Double price;
+    private Map<String, Double> restaurantMenu;
 
-    public viewMenu(String restaurant) {
-        this.restaurant = restaurant;
-        foodList = new ArrayList<>();
+    public viewMenu(String currentLocation) {
         this.currentLocation = currentLocation;
+        this.restaurantMenu = new HashMap<>();
+        this.foodName = foodName;
+        this.price = price;
     }
 
     public String getRestaurant() {
-        return restaurant;
+        return currentLocation;
     }
 
-    public void setRestaurant(String restaurant) {
-        this.restaurant = restaurant;
+    public void setRestaurant(String currentLocation) {
+        this.currentLocation = currentLocation;
     }
 
-    public void setFoodList(ArrayList<Food> foodList) {
-        this.foodList = foodList;
-    }
-
-    public void addFoodItem(String name, double price) {
-        Food newFood = new Food(name, price);
-        foodList.add(newFood);
-        System.out.println("======================================================================");
-        System.out.println("Menu item '" + name + "' added successfully");
-        System.out.println("======================================================================");
-        displayMenu(currentLocation);
-    }
-
-    public void removeFoodItem(String name) {
-        int index = -1;
-        for (int i = 0; i < foodList.size(); i++) {
-            Food food = foodList.get(i);
-            if (food.getName().equals(name)) {
-                index = i;
-                break;
-            }
-        }
-        if (index != -1) {
-            foodList.remove(index);
-            System.out.println("======================================================================");
-            System.out.println("Menu item '" + name + "' removed successfully.");
-            System.out.println("======================================================================");
-        } else {
-            System.out.println("======================================================================");
-            System.out.println("Menu item '" + name + "' not found.");
-            System.out.println("======================================================================");
-        }
-        displayMenu(currentLocation);
-    }
-
-    public void modifyFoodItem(String name, double newPrice) {
-        boolean found = false;
-        for (Food food : foodList) {
-            if (food.getName().equals(name)) {
-                food.setPrice(newPrice);
-                found = true;
-                break;
-            }
-        }
-        if (found) {
-            System.out.println("======================================================================");
-            System.out.println("Menu item '" + name + "' price modified successfully.");
-            System.out.println("======================================================================");
-        } else {
-            System.out.println("======================================================================");
-            System.out.println("Menu item '" + name + "' not found.");
-            System.out.println("======================================================================");
-        }
-    }
-
-    public void printMenu() {
-        System.out.println("==============================================================================");
-        System.out.println(getRestaurant() + "'s Menu");
-        System.out.println("\n[1] Add Food");
-        System.out.println("[2] Modify Food Price");
-        System.out.println("[3] Remove Food");
-        System.out.println("[4] Exit View Menu");
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Select : ");
-        int choice = sc.nextInt();
-        sc.nextLine();
-        switch (choice) {
-            case 1:
-                System.out.print("Enter new food name to add : ");
-                String newName = sc.nextLine();
-                System.out.print("Enter new food price : ");
-                double newPrice = sc.nextDouble();
-                addFoodItem(newName, newPrice);
-                break;
-            case 2:
-                System.out.print("Enter food name to modify : ");
-                String name = sc.nextLine();
-                System.out.print("Enter new food price : ");
-                newPrice = sc.nextDouble();
-                modifyFoodItem(name, newPrice);
-                printMenu();
-                break;
-            case 3:
-                System.out.print("Enter food name to remove : ");
-                name = sc.nextLine();
-                removeFoodItem(name);
-                break;
-            default:
-        }
-    }
-
-    public void displayMenu(String currentLocation) {
+    public void addFoodItem(String foodName, double price) {
         Map<String, Double> restaurantMenu = getMenuByRestaurant(currentLocation);
-        System.out.println("Menu for " + currentLocation + ":");
-        System.out.println("+-----------------------------------------------+-----------+");
-        System.out.println("Food                                            |   Price   |");
-        System.out.println("+-----------------------------------------------+-----------+");
-        for (Map.Entry<String, Double> entry : restaurantMenu.entrySet()) {
-            String menuItem = entry.getKey();
-            Double price = entry.getValue();
-            System.out.printf("| %-45s | $%8.2f |\n", menuItem, price);
+        if (!restaurantMenu.containsKey(foodName)) {
+            restaurantMenu.put(foodName, price);
+            System.out.println("======================================================================");
+            System.out.println("Menu item '" + foodName + "' added successfully");
+            System.out.println("======================================================================");
+        } else {
+            System.out.println("======================================================================");
+            System.out.println("Menu item '" + foodName + "' already exists.");
+            System.out.println("======================================================================");
         }
-        System.out.println("+-----------------------------------------------+-----------+");
+        displayMenu(currentLocation);
+    }
+        public void removeFoodItem (String foodName){
+            Map<String, Double> restaurantMenu = getMenuByRestaurant(currentLocation);
+           if (restaurantMenu.containsKey(foodName)) {
+               restaurantMenu.remove(foodName);
+                System.out.println("======================================================================");
+                System.out.println("Menu item '" + foodName + "' removed successfully.");
+                System.out.println("======================================================================");
+            } else {
+                System.out.println("======================================================================");
+                System.out.println("Menu item '" + foodName + "' not found.");
+                System.out.println("======================================================================");
+            }
+            displayMenu(currentLocation);
+        }
+
+        public void modifyFoodItem (String foodName,double price) {
+            Map<String, Double> restaurantMenu = getMenuByRestaurant(currentLocation);
+            if (restaurantMenu.containsKey(foodName)) {
+                restaurantMenu.put(foodName, price);
+                System.out.println("======================================================================");
+                System.out.println("Menu item '" + foodName + "' price modified successfully.");
+                System.out.println("======================================================================");
+            } else {
+                System.out.println("======================================================================");
+                System.out.println("Menu item '" + foodName + "' not found.");
+                System.out.println("======================================================================");
+            }
+            displayMenu(currentLocation);
+        }
+
+        public void displayMenu (String currentLocation){
+            Map<String, Double> restaurantMenu = getMenuByRestaurant(currentLocation);
+            System.out.println("Menu for " + currentLocation + ":");
+            System.out.println("+-----------------------------------------------+-----------+");
+            System.out.println("Food                                            |   Price   |");
+            System.out.println("+-----------------------------------------------+-----------+");
+            for (Map.Entry<String, Double> entry : restaurantMenu.entrySet()) {
+                String menuItem = entry.getKey();
+                Double price = entry.getValue();
+                System.out.printf("| %-45s | $%8.2f |\n", menuItem, price);
+            }
+            System.out.println("+-----------------------------------------------+-----------+");
+            System.out.println(getRestaurant() + "'s Menu");
+            System.out.println("\n[1] Add Food");
+            System.out.println("[2] Modify Food Price");
+            System.out.println("[3] Remove Food");
+            System.out.println("[4] Exit View Menu");
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Select : ");
+            int choice = sc.nextInt();
+            sc.nextLine();
+            switch (choice) {
+                case 1:
+                    System.out.println("=====================================================");
+                    System.out.print("Enter new food name to add : ");
+                    foodName = sc.nextLine();
+                    System.out.print("Enter new food price : $");
+                    price = sc.nextDouble();
+                    addFoodItem(foodName, price);
+                    break;
+                case 2:
+                    System.out.println("=====================================================");
+                    System.out.print("Enter food name to modify : ");
+                    foodName = sc.nextLine();
+                    System.out.print("Enter new food price : $");
+                    price = sc.nextDouble();
+                    modifyFoodItem(foodName, price);
+                    break;
+                case 3:
+                    System.out.println("=====================================================");
+                    System.out.print("Enter food name to remove : ");
+                    foodName = sc.nextLine();
+                    removeFoodItem(foodName);
+                    break;
+                case 4:
+                    System.out.println("======================================================================");
+                    return;
+                default:
+            }
     }
 }
