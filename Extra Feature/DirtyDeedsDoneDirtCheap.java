05b134b2c88e22b3e8a15470170d626eb3b51963
kerry <116733173+kerryyys@users.noreply.github.com>
2023-06-14 16:24:46 +0800

    Add files via upload

---
+++ Extra Feature/DirtyDeedsDoneDirtCheap.java
@@ -0,0 +1,84 @@
+package JOJOLands.JOJO;
+
+import java.util.*;
+
+public class DirtyDeedsDoneDirtCheap{
+
+    private Scanner sc = new Scanner(System.in);
+    private static Graph<String, Integer> map = new Graph<>();
+    List<List<String>> paths = new ArrayList<>();
+
+    public DirtyDeedsDoneDirtCheap(Graph<String, Integer> maps) {
+        this.map = maps;
+    }
+
+    public void RunDDDDC(){
+        List<List<String>> paths = new ArrayList<>();
+        List<String> currentPath = new ArrayList<>();
+
+        System.out.print("Source:");
+        String source = sc.nextLine();
+        System.out.print("\nDestination: ");
+        String destination = sc.nextLine();
+        System.out.println("\n================================================================================");
+
+        currentPath.add(source);
+        findPossiblePaths(source, destination, currentPath, paths);
+
+        sortPathsByDistance(paths);
+
+        List<List<String>> topPaths = new ArrayList<>();
+         for (int i = 0; i < Math.min(3, paths.size()); i++) {
+            topPaths.add(paths.get(i));
+        }
+
+        System.out.println();
+        System.out.println("Top Three Shortest Paths:");
+        for (int i = 0; i < Math.min(3, topPaths.size()); i++) {
+            double distance = calculateDistance(topPaths.get(i));
+            System.out.printf("%d. ", i + 1);
+            for (int j = 0; j < topPaths.get(i).size(); j++) {
+                System.out.print(topPaths.get(i).get(j));
+                if (j < topPaths.get(i).size() - 1) {
+                    System.out.print(" -> ");
+                }
+            }
+        System.out.printf(" (%.2f km)\n", distance);
+    }
+        System.out.println("================================================================================");
+        System.out.println();
+    }
+
+    // find possible paths using depth first search
+    public static void findPossiblePaths(String currentVertex, String targetVertex,List<String> currentPath, List<List<String>> paths) {
+        if (currentVertex.equals(targetVertex)) {
+            paths.add(new ArrayList<>(currentPath));
+            return;
+        }
+
+        List<String> neighbors = map.getNeighbours(currentVertex);
+        for (String neighbor : neighbors) {
+            if (!currentPath.contains(neighbor)) {
+                currentPath.add(neighbor);
+                findPossiblePaths(neighbor, targetVertex, currentPath, paths);
+                currentPath.remove(currentPath.size() - 1);
+            }
+        }
+    }
+
+    public static int calculateDistance(List<String> path) {
+        int distance = 0;
+        for (int i = 0; i < path.size() - 1; i++) {
+            String currentVertex = path.get(i);
+            String nextVertex = path.get(i + 1);
+            int edgeWeight = map.getEdgeWeight(currentVertex, nextVertex);
+
+            distance += edgeWeight;
+        }
+        return distance;
+    }
+
+     private static void sortPathsByDistance(List<List<String>> paths) {
+        paths.sort(Comparator.comparingDouble(DirtyDeedsDoneDirtCheap::calculateDistance));
+    }
+}
--- Extra Feature/TheGoldenSpirit.java
+++ Extra Feature/TheGoldenSpirit.java
@@ -1,16 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
-package AssignmentDS;
+package JOJOLands.JOJO;
 
-/**
- *
- * @author Darwi
- */
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
 class Node {
     String data;
@@ -36,7 +26,8 @@ public class TheGoldenSpirit {
         }
 
         // Check if "Jotaro Kujo" and "Jolyne Cujoh" are chosen
-        if ((n1.equals("Jotaro Kujo") && n2.equals("Jolyne Cujoh")) || (n1.equals("Jolyne Cujoh") && n2.equals("Jotaro Kujo"))) {
+        if ((n1.equals("Jotaro Kujo") && n2.equals("Jolyne Cujoh"))
+                || (n1.equals("Jolyne Cujoh") && n2.equals("Jotaro Kujo"))) {
             return root.left.left.left.left;
         }
 
@@ -100,7 +91,7 @@ public class TheGoldenSpirit {
         return false;
     }
 
-    public static void LCAJoestarFamily() {
+    public void LCAJoestarFamily() {
         TheGoldenSpirit tree = new TheGoldenSpirit();
         tree.root = new Node("George Joestar I and Mary Joestar");
         tree.root.left = new Node("Jonathan Joestar");
