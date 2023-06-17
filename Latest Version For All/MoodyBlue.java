6b8afe59d9ddefde7a4fa4f8dbd7a038dd53cabc
LEOWJSheng <118702346+LEOWJSheng@users.noreply.github.com>
2023-06-17 15:52:26 +0800

    Update MoodyBlue.java

--- Latest Version For All/MoodyBlue.java
+++ Latest Version For All/MoodyBlue.java
@@ -462,6 +462,10 @@ class SalesRecord {
     public double getPrice() {
         return price;
     }
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
 // JS:    public double getSales() {
 //         return Sales;
 //     }
