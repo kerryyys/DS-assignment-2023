d62956ba11eed604cfa854aa4c3b8247850be65e
kerry <116733173+kerryyys@users.noreply.github.com>
2023-06-18 01:38:41 +0800

    All Extra Feature Updated

--- Extra Feature/AnotherOneBiteTheDusts.java
+++ Extra Feature/AnotherOneBiteTheDusts.java
@@ -1,45 +1,80 @@
-package JOJOLands;
+package JOJOLands.JOJO;
 
 import java.util.*;
 
 public class AnotherOneBiteTheDusts {
-    
-        public void checkBiteTheClass() {
-            Scanner scanner = new Scanner(System.in);
+    public void checkBiteTheDust() {
+        Scanner sc = new Scanner(System.in);
 
-            System.out.println("Enter Yoshikage Kira's path: ");
-            String path = scanner.nextLine();
+        System.out.print("Enter Yoshikage Kira's path: ");
+        String path = sc.nextLine();
+        String[] pathArray = path.split(" > ");
+        List<String> longestPath = findLongestNonOverlappingPath(pathArray);
 
-            String[] pathArray = path.split(">");
-
-            // Identify the longest path with a duplicate
-            String longestPathWithDuplicate = "";
-            int maxLength = 0;
-            Set<String> visitedPaths = new HashSet<>();
-            boolean hasDuplicate = false;
-
-            for (int i = 0; i < pathArray.length - 1; i++) {
-                StringBuilder currentPath = new StringBuilder(pathArray[i]);
-                for (int j = i + 1; j < pathArray.length; j++) {
-                    currentPath.append(">").append(pathArray[j]);
-                    if (!visitedPaths.contains(currentPath.toString())) {
-                        visitedPaths.add(currentPath.toString());
-                    } else {
-                        hasDuplicate = true;
-                        if (currentPath.length() > maxLength) {
-                            longestPathWithDuplicate = currentPath.toString();
-                            maxLength = currentPath.length();
-                        }
-                    }
-                }
-            }
-
-            // Print the result or "try again" message
-            if (hasDuplicate) {
-                System.out.println("Bites the Dust is most likely to be activated when Kira passed through " + longestPathWithDuplicate);
-            } else {
-                System.out.println("Bites the Dust is not activated.");
-            }
-            System.exit(0);
+        if (longestPath.size() > 1) {
+            System.out.println("================================================================================");
+            System.out.print("Bites the Dust is most likely to be activated when Kira passed through ");
+            System.out.println(String.join(" > ", longestPath) + ".");
+            System.out.println("================================================================================");
+        } else {
+            System.out.println("================================================================================");
+            System.out.println("Bites the Dust is not activated.");
+            System.out.println("================================================================================");
         }
+    }
+
+    public static List<String> findLongestNonOverlappingPath(String[] locations) {
+        Map<String, Integer> pathCounts = new HashMap<>();
+        List<String> longestPath = new ArrayList<>();
+        List<String> currentPath = new ArrayList<>();
+
+        for (String location : locations) {
+            if (currentPath.contains(location)) {
+                if (currentPath.size() > longestPath.size()) {
+                    longestPath = new ArrayList<>(currentPath);
+                }
+                currentPath.subList(0, currentPath.indexOf(location) + 1).clear();
+            }
+            currentPath.add(location);
+
+            String pathString = String.join(" > ", currentPath);
+
+            if (pathCounts.containsKey(pathString)) {
+                pathCounts.put(pathString, pathCounts.get(pathString) + 1);
+            } else {
+                pathCounts.put(pathString, 1);
+            }
+        }
+
+        if (currentPath.size() > longestPath.size()) {
+            longestPath = new ArrayList<>(currentPath);
+        }
+
+        List<String> repeatedPaths = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry : pathCounts.entrySet()) {
+            if (entry.getValue() > 1 && !hasSelfOverlap(entry.getKey())) {
+                repeatedPaths.add(entry.getKey());
+            }
+        }
+
+        if (repeatedPaths.isEmpty()) {
+            return Collections.emptyList();
+        }
+
+        String longestRepeatedPath = Collections.max(repeatedPaths, Comparator.comparing(String::length));
+        return Arrays.asList(longestRepeatedPath.split(" > "));
+    }
+
+    public static boolean hasSelfOverlap(String pathString) {
+        String[] locations = pathString.split(" > ");
+        Set<String> visited = new HashSet<>();
+
+        for (String location : locations) {
+            if (!visited.add(location)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
 }
